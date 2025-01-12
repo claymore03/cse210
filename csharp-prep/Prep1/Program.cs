6ea1fe290dbc9bb227ae612f6777611360184a74
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-01-11 19:03:11 -0600

    Completed the first prep assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,25 @@
+//Name: Clayton Redding
+//Date: 01/11/2025
+//Course: CSE 210
+//Professor: Brother Parrish
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Create variables for the first name and last name.
+        string firstName;
+        string lastName;
+        
+        //Ask user to input a first name.
+        Console.WriteLine("Enter your first name: ");
+        firstName = Console.ReadLine();
+        //Ask user to input a last name.
+        Console.WriteLine("Enter your last name: ");
+        lastName = Console.ReadLine();
+        //Display the last name, then the first and last name together.
+        Console.WriteLine("Your name is " + lastName + ", " + firstName + " " + lastName + ".");
+
     }
 }
