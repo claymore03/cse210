64ee1607eff8f6f643536bbcc361e8d3a14fee54
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-01-15 19:23:55 -0600

    Completed Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to Guess My Number!");
+        
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        
+        string userInput;
+
+        Convert.ToString(number);
+        Console.WriteLine("The magic number is " + number);
+        
+        Console.Write("Enter your guess: ");
+        userInput = Console.ReadLine();
+        Console.WriteLine("You're guess was " + userInput);
+
+        int guess = int.Parse(userInput);
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Too low. Guess higher!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too high! Guess lower.");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+
+                break;
+            }
+
+            Console.Write("Enter your guess: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine("You're guess was " + userInput);
+
+            guess = int.Parse(userInput);
+        }
+
+        Console.WriteLine("Congratulations! You guessed it!");
+
     }
 }
