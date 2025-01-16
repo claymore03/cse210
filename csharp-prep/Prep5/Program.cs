f5e970bf67bfacda9220199678ab506e878c71a4
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-01-15 20:42:22 -0600

    Completed prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       DisplayWelcomeMessage();
+       string name = PromptUserName();
+       double favNum = PromptUserNumber();
+       double squaredNum = SquareNumber(favNum);
+       DisplayResults(name, squaredNum); 
+       
+       static void DisplayWelcomeMessage()
+       {
+        Console.WriteLine("Welcome to the program!");
+       }
+       
+       static string PromptUserName()
+       {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+       }
+       
+       static double PromptUserNumber()
+       {
+        Console.Write("Enter your favorite number: ");
+        double userNumber = double.Parse(Console.ReadLine());
+        return userNumber;
+       }
+       
+       double SquareNumber(double number)
+       {
+        double squared = Math.Pow(number, 2);
+        return squared;
+       }
+
+       static void DisplayResults(string name, double num)
+       {
+        Console.WriteLine(name+", the square of your number is "+num);
+       }
+
     }
 }
