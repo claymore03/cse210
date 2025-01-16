1658ca3089856724da83774c4f2fc03ee26a4eff
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-01-15 20:00:43 -0600

    Completed Prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userInput;
+        Console.WriteLine("Enter a number: ");
+        userInput = int.Parse(Console.ReadLine());
+        numbers.Add(userInput);
+
+        while (userInput != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+            numbers.Add(userInput);
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine("The sum is: " + sum);
+
+        float average = sum/numbers.Count();
+        Console.WriteLine("The average is: " + average);
+
+        int maxNum = numbers.Max();
+        Console.WriteLine("The largest number is: " + maxNum);
+        
+        // foreach (int number in numbers)
+        // {
+        //     Convert.ToString(number);
+        //     Console.WriteLine(number);
+        // }
     }
 }
