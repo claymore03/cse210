92b6ce39e459cd305cd0fd9bdd65903eaca75876
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-01-20 23:21:36 -0600

    Completed Abstraction learning activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,38 @@
+public class Job
+{
+    string _jobTitle;
+    string _companyName;
+    int _startYear;
+    int _endYear;
+    public void GetJobTitle()
+    {
+        Console.Write("Enter job title: ");
+        _jobTitle = Console.ReadLine();
+    }
+    public void GetCompanyName()
+    {
+        Console.Write("Enter the company's name: ");
+        _companyName = Console.ReadLine();
+    }
+    public void GetStartYear()
+    {
+        Console.Write("Enter start year: ");
+        _startYear = Convert.ToInt32(Console.ReadLine());
+        Convert.ToString(_startYear);
+        
+    }
+
+    public void GetEndYear()
+    {
+        Console.Write("Enter end year: ");
+        _endYear = Convert.ToInt32(Console.ReadLine());
+        Convert.ToString(_endYear);
+        Console.WriteLine();
+        
+    }
+    
+    public void DisplayJobs()
+    {
+        Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
+    }
+}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine(_name);
+        Console.WriteLine("Work History: ");
+    
+    
+        foreach (Job job in _jobs)
+            {
+                // This calls the Display method on each job
+                job.DisplayJobs();
+            }
+    }
+}
