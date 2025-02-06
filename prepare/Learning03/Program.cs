fa26413f2410bd6d827c8158282e251d7eb53b38
claymore03 <113389977+claymore03@users.noreply.github.com>
2025-02-06 12:10:41 -0600

    Completed prep 3 assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //Default fraction (1/1)
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.DisplayFractionString());
+        Console.WriteLine(f1.CalculateDecimalValue());
+
+        //Whole number in fraction form (7/1)
+        Fraction f2 = new Fraction(7);
+        Console.WriteLine(f2.DisplayFractionString());
+        Console.WriteLine(f2.CalculateDecimalValue());
+
+        //Regular Fraction
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.DisplayFractionString());
+        Console.WriteLine(f3.CalculateDecimalValue());
+
+        //Regular Fraction
+        Fraction f4 = new Fraction(2, 9);
+        Console.WriteLine(f4.DisplayFractionString());
+        Console.WriteLine(f4.CalculateDecimalValue());
+        
+
     }
 }
